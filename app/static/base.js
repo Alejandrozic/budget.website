f572c058f963ad9efeaa6f551189bce45f352e97
AZ <alejandrozic@gmail.com>
2021-03-13 14:10:07 -0500

    added switching of active class for active nav-item/nav-link

--- app/static/base.js
+++ app/static/base.js
@@ -5,3 +5,31 @@ $(function () {
     $('.offcanvas-collapse').toggleClass('open')
   })
 })
+
+/*
+    Javascript for switching active nav item/link.
+*/
+
+$(function () {
+  var curr = location.pathname;
+  $('.nav-link').each(function(){
+    var $this = $(this);
+    if ($this.attr('href') == curr){
+        $(this).addClass('active');
+    } else{
+        $(this).removeClass('active');
+    }
+  })
+})
+
+$(function () {
+  var curr = location.pathname;
+  $('.nav-item').each(function(){
+    var $this = $(this);
+    if ($this.attr('href') == curr){
+        $(this).addClass('active');
+    } else{
+        $(this).removeClass('active');
+    }
+  })
+})
